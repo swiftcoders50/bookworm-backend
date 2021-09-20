16570f5f6bb37c282581a7fa8e0d8b7c243f313d
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-20 21:03:45 +0600

    Application initialization

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const mongoose = require("mongoose");
 const homeRouter = require("./routers/homeRouter");
 const booksRouter = require("./routers/booksRouter");
 
+// application initialization
 const app = express();
 dotenv.config();
 
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database Connection Successful."))
-  .catch((err) => console.log(err));
+	.connect(process.env.MONGO_CONNECTION_STRING, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Database Connection Successful."))
+	.catch((err) => console.log(err));
 
 app.use(express.json());
 
@@ -25,5 +26,5 @@ app.use("/", homeRouter);
 app.use("/books", booksRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening to port ${process.env.PORT}.`);
+	console.log(`App listening to port ${process.env.PORT}.`);
 });
